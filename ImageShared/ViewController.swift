62bdf543047fca54a122ee290062071468cf335f
İbrhaim Halid Bayrak <ibrahimh.bayrak@gmail.com>
2023-08-05 20:17:11 +0300

    modified

--- ImageShared/ViewController.swift
+++ ImageShared/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
     
@@ -18,12 +19,44 @@ class ViewController: UIViewController {
     }
     
     @IBAction func signInClicked(_ sender: Any) {
+        if emailTextField.text != "" && passwordTextField.text != "" {
+            Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) {
+                (authdataresult, error) in
+                if error != nil {
+                    self.errorMessage(titleInput: "Hata!", messageInput: error?.localizedDescription ?? "Hata aldınız, Tekrar Deneyiniz!")
+                } else {
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+            
+        } else {
+            self.errorMessage(titleInput: "Hata!", messageInput: "Email ve Parola Giriniz!")
+        }
         
     }
     @IBAction func signUpClicked(_ sender: Any) {
-        performSegue(withIdentifier: "toFeedVC", sender: nil)
+        if emailTextField.text != "" && passwordTextField.text != "" {
+            //Kayıt olma işlemi
+            Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) {
+                authdataresutlt, error in
+                if error != nil {
+                    self.errorMessage(titleInput: "Hata!", messageInput: error?.localizedDescription ?? "Hata aldınız, Tekrar Deneyiniz!")
+                } else {
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+            
+        } else {
+            errorMessage(titleInput: "Hata!", messageInput: "Email ve Parola Giriniz!")
+        }
     }
     
+    func errorMessage(titleInput: String, messageInput: String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
     
 }
 
