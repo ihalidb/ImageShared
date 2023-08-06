b522555a07f625a4807823c2325f26c285379ebd
İbrhaim Halid Bayrak <ibrahimh.bayrak@gmail.com>
2023-08-06 15:45:46 +0300

    image upload done. get url done.

--- ImageShared/UploadViewController.swift
+++ ImageShared/UploadViewController.swift
@@ -6,16 +6,18 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseStorage
+import FirebaseAuth
 
 class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
-
+    
     @IBOutlet weak var yorumTextField: UITextField!
     @IBOutlet weak var imageView: UIImageView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         imageView.isUserInteractionEnabled = true
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(gorselSec))
         imageView.addGestureRecognizer(gestureRecognizer)
@@ -37,6 +39,25 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
     }
     
     @IBAction func uploadButtonTiklandi(_ sender: Any) {
+        let storage = Storage.storage()
+        let storageReferance = storage.reference()
+        let mediraFolder = storageReferance.child("media")
+        if let data = imageView.image?.jpegData(compressionQuality: 0.5) {
+            let imageReferance = mediraFolder.child("image.jpg")
+            imageReferance.putData(data) { (storagemetadata, error) in
+                if error != nil {
+                    print(error?.localizedDescription)
+                } else {
+                    imageReferance.downloadURL { url, error in
+                        if error == nil {
+                            let imageUrl = url?.absoluteString
+                            print(imageUrl)
+                        }
+                    }
+                }
+            }
+        }
+        
     }
     
 }
